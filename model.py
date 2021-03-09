d8c49168e16254570f90f25e928ee5ca7082fa53
LowinLi <oncwnuOcSiFTBJoInt-ZizkPejGw@git.weixin.qq.com>
2021-03-09 15:04:56 +0800

    fix batch每个span位置的简便处理

--- model.py
+++ model.py
@@ -18,39 +18,25 @@ class Roberta(nn.Module):
             self.embeddings = BertModel.from_pretrained(config.model_saved_dir)
         self.device = config.device
 
-    def forward(self, x):
+        def forward(self, x):
         input_ids, attention_mask, span1_begin, span1_end, span2_begin, span2_end = x
         embedded = self.embeddings(input_ids, attention_mask)
-        batch_size = embedded[0].shape[0]
-        max_length = embedded[0].shape[1]
-        span1_begin_emb = torch.mul(
-            torch.zeros(batch_size, max_length)
-            .to(self.device)
-            .scatter_(1, (span1_begin + 1).unsqueeze(1), 1),
-            embedded[0].permute(2, 0, 1),
-        ).sum(2)
-        span1_end_emb = torch.mul(
-            torch.zeros(batch_size, max_length)
-            .to(self.device)
-            .scatter_(1, (span1_end + 1).unsqueeze(1), 1),
-            embedded[0].permute(2, 0, 1),
-        ).sum(2)
-        span2_begin_emb = torch.mul(
-            torch.zeros(batch_size, max_length)
-            .to(self.device)
-            .scatter_(1, (span2_begin + 1).unsqueeze(1), 1),
-            embedded[0].permute(2, 0, 1),
-        ).sum(2)
-        span2_end_emb = torch.mul(
-            torch.zeros(batch_size, max_length)
-            .to(self.device)
-            .scatter_(1, (span2_end + 1).unsqueeze(1), 1),
-            embedded[0].permute(2, 0, 1),
-        ).sum(2)
-        span1_emb = (span1_begin_emb + span1_end_emb) / 2
-        span2_emb = (span2_begin_emb + span2_end_emb) / 2
-        cosine = torch.cosine_similarity(span1_emb, span2_emb, dim=0)
-        # output = (output + 1)/2
+        indexs = torch.cat(
+            (
+                span1_begin.unsqueeze(1),
+                span1_end.unsqueeze(1) - 1,
+                span2_begin.unsqueeze(1),
+                span2_end.unsqueeze(1) - 1,
+            ),
+            1,
+        )
+        indexs = indexs.unsqueeze(2).repeat(1, 1, embedded[0].shape[2])
+        span_vecs = torch.gather(embedded[0], dim=1, index=indexs)
+        cosine = torch.cosine_similarity(
+            (span_vecs[:, 0, :] + span_vecs[:, 1, :]) / 2,
+            (span_vecs[:, 2, :] + span_vecs[:, 3, :]) / 2,
+            dim=1,
+        )
         output = torch.cat(tuple([1 - cosine.unsqueeze(1), cosine.unsqueeze(1)]), 1)
         return nn.functional.softmax(output, dim=1)
 
